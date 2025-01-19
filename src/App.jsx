d06c54213a252f2897a612df7efcda43afa4d14e
pratikfulkar <pratikfulkar8698@gmail.com>
2025-01-19 22:33:07 +0530

    route issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
 import LandingPage from './pages/LandingPage';
-// import AboutUs from './pages/AboutUs';
-import Services from './pages/Services';
-// import Contact from './pages/Contact';
 import AddUser from './pages/AddUser';
 
 const App = () => {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
-        {/* <Header /> */}
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          {/* <Route path="/about" element={<AboutUs />} /> */}
-          <Route path="/services" element={<Services />} />
-          {/* <Route path="/contact" element={<Contact />} /> */}
           <Route path="/adduser" element={<AddUser />} />
         </Routes>
       </div>
@@ -24,3 +16,4 @@ const App = () => {
 };
 
 export default App;
+
